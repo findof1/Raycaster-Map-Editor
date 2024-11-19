3afb7707800185c671fb1c1ff557b7b888daa9d7
Findof <joeargon21@gmail.com>
2024-11-18 20:56:10 -0500

    Multithreaded commands

--- main.cpp
+++ main.cpp
@@ -1,15 +1,96 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <stdio.h>
 #include <vector>
 #include <fstream>
+#include <thread>
+#include <atomic>
+#include <string>
+#include <mutex>
+#include <optional>
+
+struct Command
+{
+    std::string cmd;
+
+    enum DataType
+    {
+        None,
+        LoadData,
+        UnloadData
+    } dataType = DataType::None;
+
+    union Data
+    {
+        struct LoadData
+        {
+            std::string fileName;
+            LoadData() : fileName() {}
+            LoadData(const std::string &name) : fileName(name) {}
+            ~LoadData() {}
+        } loadData;
+        struct UnloadData
+        {
+            int width;
+            int height;
+            UnloadData() : width(0), height(0) {}
+            UnloadData(int w, int h) : width(w), height(h) {}
+            ~UnloadData() {}
+        } unloadData;
+        Data() {}
+        ~Data() {}
+    };
+
+    void setLoadData(const std::string &fileName)
+    {
+        clearData();
+        new (&data->loadData) Data::LoadData(fileName);
+        dataType = DataType::LoadData;
+    }
+
+    void setUnloadData(int width, int height)
+    {
+        clearData();
+        new (&data->unloadData) Data::UnloadData(width, height);
+        dataType = DataType::UnloadData;
+    }
+
+    void clearData()
+    {
+        if (data)
+        {
+            if (dataType == DataType::LoadData)
+                data->loadData.~LoadData();
+            else if (dataType == DataType::UnloadData)
+                data->unloadData.~UnloadData();
+
+            data.reset();
+        }
+        dataType = DataType::None;
+    }
+
+    ~Command()
+    {
+        clearData();
+    }
+
+    std::optional<Data> data;
+};
 
 int mapWidth;
 int mapHeight;
 int cellType = 1;
+std::atomic<bool> running(true);
+std::mutex commandMutex;
+Command command;
+
+std::vector<SDL_Texture *> textures;
+std::vector<std::string> texturePaths = {"./textures/texture-1.png", "./textures/texture-2.png", "./textures/texture-3.png", "./textures/texture-4.png", "./textures/texture-5.png", "./textures/texture-6.png"};
 
 void serialize(int mapWidth, int mapHeight, const std::vector<int> &map, const std::vector<int> &mapFloors, const std::vector<int> &mapCeiling, const std::string &filename)
 {
@@ -66,6 +147,43 @@ void deserialize(int *mapWidth, int *mapHeight, std::vector<int> *map, std::vect
     else
     {
         std::cerr << "Error opening file for reading.\n";
+    }
+}
+
+void consoleCommands()
+{
+    while (running)
+    {
+        std::string input;
+        std::cin >> input;
+        command.clearData();
+        command.dataType = Command::DataType::None;
+        if (input == "load")
+        {
+            command.dataType = Command::DataType::LoadData;
+            std::string fileName;
+            std::cout << "Enter a filename: ";
+            std::cin >> fileName;
+
+            command.setLoadData(fileName);
+        }
+
+        if (input == "unload")
+        {
+            int w, h;
+            command.dataType = Command::DataType::UnloadData;
+            std::cout << "Enter the width of the map: ";
+            std::cin >> w;
+            std::cout << std::endl;
+            std::cout << "Enter the height of the map: ";
+            command.clearData();
+            std::cin >> h;
+
+            command.setUnloadData(w, h);
+        }
+
+        std::lock_guard<std::mutex> lock(commandMutex);
+        command.cmd = input;
     }
 }
 
@@ -126,10 +244,43 @@ int main()
         SDL_Quit();
         return 1;
     }
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
+    {
+        printf("IMG_Init Error: %s\n", IMG_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        printf("Loading texture: %s\n", texturePaths[i].c_str());
 
-    bool running = true;
+        SDL_Surface *surface = IMG_Load(texturePaths[i].c_str());
+        if (!surface)
+        {
+            printf("IMG_Load Error for texture %d: %s\n", i, IMG_GetError());
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
+
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+        if (!texture)
+        {
+            printf("SDL_CreateTextureFromSurface Error for texture %d: %s\n", i, SDL_GetError());
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
+        textures.emplace_back(texture);
+    }
     float width = 700 / mapWidth;
     float height = 700 / mapHeight;
+    std::thread consoleThread(consoleCommands);
     while (running)
     {
 
@@ -182,10 +333,6 @@ int main()
         {
             cellType = 6;
         }
-        if (keystate[SDL_SCANCODE_7])
-        {
-            cellType = 7;
-        }
         if (keystate[SDL_SCANCODE_Q])
         {
             currentMap = &map;
@@ -206,47 +353,74 @@ int main()
         {
             for (int y = 0; y < mapHeight; y++)
             {
-                SDL_FRect square;
+                SDL_Rect square;
                 square.w = width - 1;
                 square.h = height - 1;
                 square.x = x * width + 1;
                 square.y = y * height + 1;
-                switch (currentMap->at(x + y * mapWidth))
+                if (currentMap->at(x + y * mapWidth) - 1 >= 0)
                 {
-                case 0:
-                    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
-                    break;
-                case 1:
-                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-                    break;
-                case 2:
-                    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-                    break;
-                case 3:
-                    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-                    break;
-                case 4:
-                    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-                    break;
-                case 5:
-                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-                    break;
-                case 6:
-                    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-                    break;
-                case 7:
-                    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                    break;
+                    SDL_RenderCopy(renderer, textures[currentMap->at(x + y * mapWidth) - 1], NULL, &square);
                 }
-                SDL_RenderDrawRectF(renderer, &square);
+                else
+                {
+                    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+                }
+
+                SDL_RenderDrawRect(renderer, &square);
             }
         }
 
         SDL_RenderPresent(renderer);
 
+        {
+            std::lock_guard<std::mutex> lock(commandMutex);
+            if (!command.cmd.empty())
+            {
+                std::cout << "Command received: " << command.cmd << std::endl;
+                if (command.cmd == "quit")
+                {
+                    running = false;
+                }
+                else if (command.cmd == "save")
+                {
+                    serialize(mapWidth, mapHeight, map, mapFloors, mapCeiling, "map.dat");
+                }
+                else if (command.cmd == "load")
+                {
+                    deserialize(&mapWidth, &mapHeight, &map, &mapFloors, &mapCeiling, command.data->loadData.fileName);
+                    width = 700 / mapWidth;
+                    height = 700 / mapHeight;
+                }
+                else if (command.cmd == "unload")
+                {
+                    mapWidth = command.data->unloadData.width;
+                    mapHeight = command.data->unloadData.height;
+
+                    map.clear();
+                    mapFloors.clear();
+                    mapCeiling.clear();
+
+                    map.resize(mapWidth * mapHeight, 0);
+                    mapFloors.resize(mapWidth * mapHeight, 0);
+                    mapCeiling.resize(mapWidth * mapHeight, 0);
+
+                    std::fill(map.begin(), map.end(), 0);
+                    std::fill(mapFloors.begin(), mapFloors.end(), 0);
+                    std::fill(mapCeiling.begin(), mapCeiling.end(), 0);
+                    width = 700 / mapWidth;
+                    height = 700 / mapHeight;
+                }
+                command.cmd.clear();
+            }
+        }
+
         SDL_Delay(16);
     }
 
+    consoleThread.join();
+
+    IMG_Quit();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
